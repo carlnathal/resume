f811b21be371ab48c6c5f445f961ff8ebaceeaa3
beomjo <rlaqjawh46@naver.com>
2023-12-29 04:57:45 +0900

    sixth commit

--- src/components/WrapComponent.jsx
+++ src/components/WrapComponent.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./wrap2.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFaceSmile } from "@fortawesome/free-regular-svg-icons";
 import { faFile } from "@fortawesome/free-regular-svg-icons";
 
 export default function WrapComponent() {
   return (
-    <div id="wrap">
+    <div id="wrap2">
       <div className="container">
         <div className="gap">
           <div className="content">
